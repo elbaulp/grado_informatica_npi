a8af5b90a405194c6dd9087e667b8469e1587c7d
Alejandro Alcalde <algui91@gmail.com>
2016-02-08 20:17:46 +0100

    Changed pref title when patter established

--- Android/PhotoGesture/app/src/main/java/com/elbauldelprogramador/photogesture/app/MainFragment.java
+++ Android/PhotoGesture/app/src/main/java/com/elbauldelprogramador/photogesture/app/MainFragment.java
@@ -6,19 +6,27 @@
 package com.elbauldelprogramador.photogesture.app;
 
 import com.elbauldelprogramador.photogesture.R;
+import com.elbauldelprogramador.photogesture.util.PatternLockUtils;
 import com.elbauldelprogramador.photogesture.util.PreferenceContract;
 import com.elbauldelprogramador.photogesture.util.PreferenceUtils;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
 public class MainFragment extends PreferenceFragmentCompat
         implements SharedPreferences.OnSharedPreferenceChangeListener {
 
+    Preference mPf;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.preferences_main);
+
+        mPf = findPreference("pl");
+
+        setPreferenceTitle();
     }
 
     @Override
@@ -27,6 +35,8 @@ public class MainFragment extends PreferenceFragmentCompat
 
         PreferenceUtils.getPreferences(getActivity())
                 .registerOnSharedPreferenceChangeListener(this);
+
+        setPreferenceTitle();
     }
 
     @Override
@@ -42,6 +52,14 @@ public class MainFragment extends PreferenceFragmentCompat
         switch (key) {
             case PreferenceContract.KEY_THEME:
                 getActivity().recreate();
+        }
+    }
+
+    private void setPreferenceTitle() {
+        if (PatternLockUtils.hasPattern(getContext())) {
+            mPf.setTitle(R.string.pref_title_take_photo);
+        } else {
+            mPf.setTitle(R.string.pref_title_pattern_lock);
         }
     }
 }
